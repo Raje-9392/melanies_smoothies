1649a3a8b9d29bf71db5370f169f50da005fd365
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 15:37:35 +0530

    Update streamlit_app.py
    Added the st.write("Session initialized:", session)

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 from snowflake.snowpark.functions import col
 
 session = get_active_session()
+st.write("Session initialized:", session)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
