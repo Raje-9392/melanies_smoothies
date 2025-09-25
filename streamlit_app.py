7e53d18312ad5b16983ec92e66930f8ad88a349a
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 16:00:56 +0530

    Update streamlit_app.py
    Added the comment import requests
    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
    st.text(smoothiefroot_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
    # st.write(ingredients_string)    
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
