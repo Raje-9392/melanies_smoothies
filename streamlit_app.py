6ce714be11e5ab5123a0cad8a3b274bcad500023
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 17:34:17 +0530

    Update streamlit_app.py
    added the two lines into the for fruit chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark import Session
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -12,8 +13,8 @@ st.write(
 cnx = st.connection("snowflake")
 session=cnx.session()
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(
     data=smoothiefroot_response.json(),
@@ -50,6 +51,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(
+    data=smoothiefroot_response.json(),
+    use_container_width=True
+)
    #st.write(ingredients_string)    
 
 
