0d2055d35e281d2b3e5345817f00b77c355da14f
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 17:08:54 +0530

    Update streamlit_app.py
    added the
     ingredients_string = st.text_input("Enter ingredients")
    name_on_order = st.text_input("Enter your name")

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-   st.write(ingredients_string)    
-
+   #st.write(ingredients_string)    
+ingredients_string = st.text_input("Enter ingredients")
+name_on_order = st.text_input("Enter your name")
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
       values('""" + ingredients_string + """','"""+name_on_order+ """')"""
    # st.write(my_insert_stmt)
