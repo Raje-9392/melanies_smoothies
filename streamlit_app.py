87f5d8c450db294365137ac8f645f7ba07861daf
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 18:07:39 +0530

    Update streamlit_app.py
    Added some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd-df)                                                                                         
 st.stop()
                                                                                             
-ingredients_list=st.multiselect
+ingredients_list=st.multiselect(
  #the below one is attach od above line left of multiselector                                                                                           
 #( 'chose up to 5 imgredients:',my_dataframe)
 
