66ec0845502ee18d7650992929d2988569773013
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 15:22:47 +0530

    Update streamlit_app.py
    added the streamlit.title

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write(
   """Choose the fruit you want in your custom Smoothie
   """)
 import streamlit as st
-streamlit.title('🥤Customize Your Smoothie!🥤')
+streamlit.title("🥤Customize Your Smoothie!🥤")
 #import streamlit as st
 
 #title = st.text_input('Movie title', 'Life of Brian')
