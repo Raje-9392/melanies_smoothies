df4e9ee2c0b5c711b3260e0cea8b35eb8b50d354
Raje-9392 <rajemedagam555@gmail.com>
2025-09-25 17:11:33 +0530

    Update streamlit_app.py
    Add the st.success("✅ Order placed successfully! (200 OK)")

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
    #st.write(ingredients_string)    
-ingredients_string = st.text_input("Enter ingredients")
-name_on_order = st.text_input("Enter your name")
+st.success("✅ Order placed successfully! (200 OK)")
+
+
+
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
       values('""" + ingredients_string + """','"""+name_on_order+ """')"""
    # st.write(my_insert_stmt)
