e4e73581bf0c5f39f63af51ac0b2e8b0d33f94b1
Raje-9392 <rajemedagam555@gmail.com>
2025-09-26 10:33:42 +0530

    Update streamlit_app.py
    Added the line-ingredients_string = ",".join(selected_ingredients)

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if ingredients_list:
    #st.write(ingredients_string)    
 
 
-
+ingredients_string = ",".join(selected_ingredients)
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
          values('""" + ingredients_string + """','"""+name_on_order+ """')"""
    # st.write(my_insert_stmt)
